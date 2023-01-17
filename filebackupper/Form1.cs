7f596524c6ee052b965cb31fb79d645dc5c5bda3
fides <jori.inberg@outlook.com>
2023-01-17 19:14:02 +0200

    Added files

---
+++ filebackupper/Form1.cs
@@ -0,0 +1,40 @@
+namespace filebackupper
+{
+    public partial class Backup : Form
+    {
+        public Backup()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
